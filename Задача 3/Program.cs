05e19455e3f2e18a18c074d3e69f3fd438c418bf
Holdoenko <xoldai3005@mail.ru>
2022-11-28 15:28:55 +0300

    Задача 3 исправил ошибку

--- Задача 3/Program.cs
+++ Задача 3/Program.cs
@@ -1,8 +1,7 @@
 ﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 Console.WriteLine("Введите число от 1 до 7: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if(number < 6)
-if(number > 0)
+if(number > 0 && number < 6)
 {
     Console.WriteLine("Рабочий день, где 1-Понедельник, 2-Вторник, 3-Среда, 4-Четверг, 5-Пятница");
 }
@@ -13,6 +12,6 @@ else if(number == 6)
 else if(number == 7)
 {
     Console.WriteLine("Выходной день, Воскресенье");
-}                                                          
+}                                                       
 else
 Console.WriteLine("Введено не верное число");
